793d5f0be6e3b0d7823c863bd3d62e9bc85be742
Cid Lopes <alannettto@gmail.com>
2022-03-07 17:48:05 +0200

    Split shopware dockerfiles and add run script to copy platform files from container to host for IDE completion

---
+++ shopware/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if repo is clonned
+if [ ! "$(ls -A nosto-shopware)" ]; then
+    git clone git@github.com:Nosto/nosto-shopware.git
+fi
+
+if [ ! "$(ls -A shopware_root)" ]; then
+    mkdir shopware_root
+    echo "Copying shopware root files to host storage for IDE completion, this will take a while..."
+    docker run -i --name shopware_root_code_gen \
+        --volume=$(pwd)/shopware_root:/var/www/html/shopware_copy \
+         nosto/shopware:5.6.2 \
+         mv "/var/www/html/shopware/" "/var/www/html/shopware_copy"
+
+    docker rm -f shopware_root_code_gen
+fi
+
+docker-compose up --build
