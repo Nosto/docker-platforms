3a967339cd071a46a36543645604d8e16ec1ec45
Cid Lopes <alannettto@gmail.com>
2022-03-08 14:55:21 +0200

    Shopware - Add SCA tools, fix dockerfile, add run.sh and copy_root_to_host functions

--- shopware/run.sh
+++ shopware/run.sh
@@ -5,17 +5,5 @@ if [ ! "$(ls -A nosto-shopware)" ]; then
     git clone git@github.com:Nosto/nosto-shopware.git
 fi
 
-docker build -t nosto/shopware:5.6.2 shopware-base --no-cache
-
-if [ ! "$(ls -A shopware_root)" ]; then
-    mkdir shopware_root
-    echo "Copying shopware root files to host storage for IDE completion, this will take a while..."
-    docker run -i --name shopware_root_code_gen \
-        --volume=$(pwd)/shopware_root:/var/www/html/shopware_copy \
-         nosto/shopware:5.6.2 \
-         mv "/var/www/html/shopware/" "/var/www/html/shopware_copy"
-
-    docker rm -f shopware_root_code_gen
-fi
-
 docker-compose up --build -d
+./copy_root_to_host.sh
