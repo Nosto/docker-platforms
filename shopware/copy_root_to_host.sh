a0c91d4c62078b8c4479c0fb64dc48c8efa566b7
Cid Lopes <alannettto@gmail.com>
2022-03-16 13:36:54 +0200

    Build locally instead of pulling image

--- shopware/copy_root_to_host.sh
+++ shopware/copy_root_to_host.sh
@@ -5,7 +5,7 @@ if [ ! "$(ls -A shopware_root)" ]; then
     echo "Copying shopware root files to host storage for IDE completion, this will take a while..."
     docker run -i --name shopware_root_code_gen \
         --volume=$(pwd)/shopware_root:/var/www/html/shopware_copy \
-         nosto/shopware:5.6.2 \
+         nosto-shopware \
          mv "/var/www/html/shopware/" "/var/www/html/shopware_copy"
 
     docker rm -f shopware_root_code_gen
