3b0ea0fdec55a022fc13acc9f6bc3db4eac64127
Cid Lopes <alannettto@gmail.com>
2022-10-20 12:36:55 +0300

    Prestashop - Fix mysql hosto

--- prestashop/scripts/init-and-run.sh
+++ prestashop/scripts/init-and-run.sh
@@ -19,14 +19,14 @@ if [ ! -f /var/www/html/prestashop/.installed ]; then
     --db_password=${MYSQL_PASSWORD} \
     --email=${ADMIN_EMAIL} \
     --password=${ADMIN_PASSWORD} \
-    --db_create=0 \
-    --db_clear=0 \
+    --db_create=1 \
+    --db_clear=1 \
     --newsletter=0 \
     --send_email=0
 
   cd /var/www/html/prestashop && rm -rf install && mv admin adminn0st0
 
-  mysql -h mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} \
+  mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} \
         -D ${MYSQL_DATABASE} \
         -Bse 'UPDATE ps_configuration SET value = 0 WHERE name = "PS_CANONICAL_REDIRECT"; UPDATE ps_configuration SET value = 0 WHERE name = "PS_SSL_ENABLED"; INSERT INTO ps_configuration (name,value) VALUES ("PS_SSL_ENABLED_EVERYWHERE","0");'
   
